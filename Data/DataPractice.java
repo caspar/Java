c977d3279a9a798a7c11cd4ebaa8b1143f4129cc
Caspar Lant <casparlant@gmail.com>
2014-04-24 16:55:13 -0400

    Caspar

---
+++ Data/DataPractice.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+//Caspar Lant
+
+public class DataPractice{
+    
+    public int average(int[] array){
+
+    }
+
+    public double average(double[] array){
+
+    }
+
+    public int quadratic(int a, int b, int c){
+    }
+
+    
+    public static void main(String[] args){
+	DataPractice D = new DataPractice();
+	System.out.println(root(25));
+	
+	
+    }
+
+}
